22d047c6f6820de37b0b13464c66b23efd186686
Fernando Wagner <58955010+fwmpa@users.noreply.github.com>
2023-03-07 08:20:10 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,14 +1,12 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
-const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjFiYmZmYmU2LTQzYjgtNDUyZC1hODc4LTZjNTdlNDk5NDIzMi0xNjc4MTg2MDUyMTAzIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiYjEwNjE0MTgtMThkOC00Mzk2LWI2NDQtOTg5NGYzMGZjZThiIiwidHlwZSI6InQifQ.mnkcT7IRFw5puNVObTs6j4p3JAmv4WTtGDpoqy1CH0E'
-
 cypress.run({
   // specs to run here
 })
 .then((results) => {
   const args = {
-    target: TOKEN,
+    target: process.env.TARGET_TOKEN_ELECTRON,
   }
   tesults.results(results, args);
 })
